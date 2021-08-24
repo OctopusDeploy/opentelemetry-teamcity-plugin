8229d75de5bf1e3398ebdd25b8f7b0b34e3afe5a
Cassandra Tait <tait.cassandra@gmail.com>
2021-08-24 18:01:51 +1000

    added LogMasker Tests for PR suggested test cases

--- server/src/test/java/com/octopus/teamcity/opentelemetry/server/LogMaskerTest.java
+++ server/src/test/java/com/octopus/teamcity/opentelemetry/server/LogMaskerTest.java
@@ -2,41 +2,91 @@ package com.octopus.teamcity.opentelemetry.server;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Locale;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LogMaskerTest {
 
     @Test
-    void testMaskLowerCase() {
+    void LowerCaseApiKeyGetsMasked() {
         int APIKEY_LENGTH = 31;
         String generatedString = RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH).toLowerCase(Locale.ROOT);
+        assertFalse(LogMasker.mask(generatedString).contains(generatedString));
         assertEquals(LogMasker.API_KEY_REPLACEMENT_REGEX, LogMasker.mask(generatedString));
     }
 
     @Test
-    void testMaskUpperCase() {
+    void UpperCaseApiKeyNotMasked() {
         int APIKEY_LENGTH = 31;
         String generatedString = RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH).toUpperCase(Locale.ROOT);
+        assertTrue(LogMasker.mask(generatedString).contains(generatedString));
         assertNotEquals(LogMasker.API_KEY_REPLACEMENT_REGEX, LogMasker.mask(generatedString));
     }
 
-    @Test
-    void testMaskLengthShort() {
-        Random random = new Random();
-        int API_KEY_LENGTH = random.nextInt(30);
-        String generatedString = RandomStringUtils.randomAlphanumeric(API_KEY_LENGTH).toUpperCase(Locale.ROOT);
+    @ParameterizedTest
+    @ValueSource(ints = { 31, 40, 70, 90, 100 })
+    void ApiKeyMaskedForLowerCaseStringLengthGreaterThan31(int apiKeyLength) {
+        String generatedString = RandomStringUtils.randomAlphanumeric(apiKeyLength).toLowerCase(Locale.ROOT);
+        assertFalse(LogMasker.mask(generatedString).contains(generatedString));
+        assertTrue(LogMasker.mask(generatedString).contains(LogMasker.API_KEY_REPLACEMENT_REGEX));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 10, 20, 25, 30 })
+    void ApiKeyNotMaskedForLowerCaseStringLengthLessThan31(int apiKeyLength) {
+        String generatedString = RandomStringUtils.randomAlphanumeric(apiKeyLength).toLowerCase(Locale.ROOT);
+        assertTrue(LogMasker.mask(generatedString).contains(generatedString));
+        assertFalse(LogMasker.mask(generatedString).contains(LogMasker.API_KEY_REPLACEMENT_REGEX));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 10, 20, 70, 90, 150 })
+    void ApiKeyNotMaskedForUpperCaseStringLengthNotEqualTo31(int apiKeyLength) {
+        String generatedString = RandomStringUtils.randomAlphanumeric(apiKeyLength).toUpperCase(Locale.ROOT);
+        assertTrue(LogMasker.mask(generatedString).contains(generatedString));
         assertNotEquals(LogMasker.API_KEY_REPLACEMENT_REGEX, LogMasker.mask(generatedString));
     }
 
-    @Test
-    void testMaskLengthLong() {
-        Random random = new Random();
-        int API_KEY_LENGTH = random.nextInt(100) + 31;
-        String generatedString = RandomStringUtils.randomAlphanumeric(API_KEY_LENGTH).toUpperCase(Locale.ROOT);
+    @ParameterizedTest
+    @ValueSource(chars = { '%', '$', '%', '#', '@', '!', '\n', ' ' })
+    void ApiKeyNotMaskedForStringWithSpecialChars(char specialChar) {
+        int APIKEY_LENGTH_FIRST_SPLIT = 15;
+        int APIKEY_LENGTH_SECOND_SPLIT = 16;
+        String generatedString = RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH_FIRST_SPLIT).toLowerCase(Locale.ROOT) + specialChar + RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH_SECOND_SPLIT).toLowerCase(Locale.ROOT);
+        assertTrue(LogMasker.mask(generatedString).contains(generatedString));
         assertNotEquals(LogMasker.API_KEY_REPLACEMENT_REGEX, LogMasker.mask(generatedString));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 10, 20, 70, 90, 150 })
+    void ApiKeyMaskedWhenContainedInStringBlock(int blockSetLength) {
+        int APIKEY_LENGTH = 31;
+        String generatedApiKey = RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH).toLowerCase(Locale.ROOT);
+        String newLine = System.getProperty("line.separator");
+        String textBlock = String.join(newLine,
+                RandomStringUtils.randomAlphanumeric(blockSetLength).toLowerCase(Locale.ROOT),
+                generatedApiKey,
+                RandomStringUtils.randomAlphanumeric(blockSetLength).toLowerCase(Locale.ROOT),
+                RandomStringUtils.randomAlphanumeric(blockSetLength).toLowerCase(Locale.ROOT));
+        assertFalse(LogMasker.mask(textBlock).contains(generatedApiKey));
+        assertTrue(LogMasker.mask(textBlock).contains(LogMasker.API_KEY_REPLACEMENT_REGEX));
+    }
+
+    @Test
+    void ApiKeyNotMaskedWhenSplitInStringBlock() {
+        int APIKEY_LENGTH = 31;
+        String generatedApiKey = RandomStringUtils.randomAlphanumeric(APIKEY_LENGTH).toLowerCase(Locale.ROOT);
+        String newLine = System.getProperty("line.separator");
+        String textBlock = String.join(newLine,
+                generatedApiKey.substring(10),
+                generatedApiKey.substring(11, 20),
+                generatedApiKey.substring(21, 30),
+                generatedApiKey.substring(31));
+        assertFalse(LogMasker.mask(textBlock).contains(generatedApiKey));
+        assertFalse(LogMasker.mask(textBlock).contains(LogMasker.API_KEY_REPLACEMENT_REGEX));
     }
 }

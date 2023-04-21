d2f8bc52efcf5d59913486584fbcae07c0663602
Fabio Antunes <fabio.antunes@thetrainline.com>
2023-04-21 16:36:12 +0100

    fix: comment otelHoneycombApiKey to prevent passing null to EncryptUtil.unscramble 😎

--- server/src/main/java/com/octopus/teamcity/opentelemetry/server/ProjectConfigurationTab.java
+++ server/src/main/java/com/octopus/teamcity/opentelemetry/server/ProjectConfigurationTab.java
@@ -68,7 +68,8 @@ public class ProjectConfigurationTab extends EditProjectTab {
             model.put("otelEndpoint", params.get(PROPERTY_KEY_ENDPOINT));
             model.put("otelHoneycombTeam", params.get(PROPERTY_KEY_HONEYCOMB_TEAM));
             model.put("otelHoneycombDataset", params.get(PROPERTY_KEY_HONEYCOMB_DATASET));
-            model.put("otelHoneycombApiKey", RSACipher.encryptDataForWeb(EncryptUtil.unscramble(params.get(PROPERTY_KEY_HONEYCOMB_APIKEY))));
+            // model.put("otelHoneycombApiKey",
+            // RSACipher.encryptDataForWeb(EncryptUtil.unscramble(params.get(PROPERTY_KEY_HONEYCOMB_APIKEY))));
 
             var headers = new ArrayList<HeaderDto>();
             params.forEach((k,v)->{
